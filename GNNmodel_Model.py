b7e4f0d04ddbcacc545cf9755a6135c822c90d92
Jellavdven <j.v.d.ven4@student.tue.nl>
2026-06-23 17:18:50 +0200

    Add files via upload

---
+++ GNNmodel_Model.py
@@ -0,0 +1,76 @@
+# The GNN model that will be trained 
+
+import torch
+from torch.nn import ModuleList, Linear
+import torch.nn.functional as F
+import torch_geometric
+from torch_geometric.nn import PNAConv, BatchNorm, global_mean_pool, PairNorm
+from torch_geometric.utils import degree
+
+
+# Compute degree histogram over training data
+def pre_deg(pre_data):
+
+    all_degrees = []
+
+    for data in pre_data:
+        row, col = data.edge_index
+
+        # Sum in both directions since edges are undirectional
+        d = degree(row, num_nodes=data.num_nodes, dtype=torch.long) + \
+            degree(col, num_nodes=data.num_nodes, dtype=torch.long)
+
+        all_degrees.append(d)
+
+    all_degrees = torch.cat(all_degrees)
+    return torch.bincount(all_degrees)
+    
+
+# Class that defines the GNN
+class myPNA(torch.nn.Module):
+    def __init__(self, data_list, in_channels, hidden_channels, num_layers):
+        super(myPNA, self).__init__()
+
+        # initialize parameters for PNA
+        deg=pre_deg(pre_data=data_list)
+        print(deg)
+        aggregators = ['sum','mean','std','max','min', 'var']
+        scalers = ['identity', 'amplification', 'attenuation', 'linear', 'inverse_linear']
+
+        # initialize module list
+        self.convs = ModuleList()
+        self.batch_norms = ModuleList()
+
+        # first layer
+        conv = PNAConv(in_channels=in_channels, out_channels=hidden_channels, edge_dim=1,
+                       aggregators=aggregators, scalers=scalers, deg=deg,
+                       towers=3, pre_layers=1, post_layers=1,
+                       divide_input=False) 
+        self.convs.append(conv)
+        self.batch_norms.append(BatchNorm(hidden_channels))
+
+        # hidden layers
+        for _ in range(num_layers-1):
+            conv = PNAConv(in_channels=hidden_channels, out_channels=hidden_channels, edge_dim=1,
+                           aggregators=aggregators, scalers=scalers, deg=deg,
+                           towers=3, pre_layers=1, post_layers=1,
+                           divide_input=False)  
+            self.convs.append(conv)
+            self.batch_norms.append(BatchNorm(hidden_channels))
+        
+        # last linear layer
+        self.lin = Linear(hidden_channels + 1, 1)
+
+    def forward(self, x, edge_index, edge_attr, batch, area_frac):
+        edge_attr = edge_attr.view(-1, 1)
+        for conv, batch_norm in zip(self.convs, self.batch_norms):
+            x = conv(x, edge_index, edge_attr)
+            x = batch_norm(x)
+            x = F.relu(x)
+
+        x = global_mean_pool(x, batch)
+        if area_frac.dim() == 1:
+            area_frac = area_frac.view(-1, 1)  # [num_graphs, 1]
+        x = torch.cat([x, area_frac], dim=1)
+        x = self.lin(x)
+        return F.softplus(x)
---
+++ GNNmodel_Visuals.py
@@ -0,0 +1,76 @@
+# Code that defines functions for visualizing the data
+
+import torch
+import numpy as np
+import scipy.stats
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import networkx as nx
+
+def visualize_snapshot_cell(data_list, idx, name):
+    """
+    data_list: list of input data that is of shape (snapshot, [Torch data object])
+    idx: list of indices within data_list that you want to visualize
+    """
+    for i in idx:
+        cell_ids = data_list[i].voronoi_equivalent
+
+        plt.figure()
+        plt.imshow(cell_ids)
+        plt.savefig(f'training_results_batch3_[norm]/{name}_ssCell_idx{i}.png', dpi=200)
+        plt.close()
+
+def visualize_snapshot_nucleus(data_list, idxz, name):
+    """
+    data_list: list of input data that is of shape (snapshot, [Torch data object])
+    idx: list of indices within data_list that you want to visualize
+    """
+    for i in idx:
+        cell_ids = data_list[i].voronoi_equivalent
+
+        plt.figure()
+        plt.imshow(cell_ids % 2)
+        plt.savefig(f'training_results_batch3_[norm]/{name}_ssNucleus_idx{i}.png', dpi=200)
+        plt.close()
+
+def visualize_snapshot_graph(data_list, idx, name):
+    """
+    data_list: list of Torch Geometric data objects
+    idx: indices to visualize
+    """
+    for i in idx:
+        edges = data_list[i].edge_index
+        nodes = data_list[i].cell_pos
+ 
+        nodes_np = nodes.cpu().numpy()
+        edges_np = edges.cpu().numpy().T.astype(int)
+        
+        G = nx.Graph()
+        G.add_nodes_from(range(len(nodes_np)))
+        G.add_edges_from(edges_np)
+        nodes_rot = np.column_stack((nodes_np[:,1], -nodes_np[:,0]))
+        pos = dict(enumerate(nodes_rot))
+        
+        plt.figure()
+        nx.draw(G, pos, with_labels=True)
+
+        plt.savefig(f'training_results_batch3_[norm]/{name}_graph_idx{i}.png', dpi=200)
+        plt.close()
+
+# see what linear regression on the nucleus area fraction can do
+def lin_regression(data_list_test):
+    test_y_values = []
+    test_phi_values = []
+    
+    for data in data_list_test:
+        test_y_values.append(data.y.item())
+        test_phi_values.append(data.area_frac.item())
+
+    # linear fitting
+    test_phi_values = np.array(test_phi_values)
+    slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(test_phi_values, test_y_values)
+    predicted_y_values = slope * test_phi_values + intercept
+    correlation = r_value
+    mean_squared_error = np.mean((predicted_y_values - test_y_values) ** 2)
+
+    return correlation, mean_squared_error, p_value
